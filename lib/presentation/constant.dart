86b6d2a64b6a4eb9648fb2e8149137a9f9b49c0a
cherys <huoda@icbcasia.com>
2021-04-25 00:19:13 +0800

    替换了主次颜色,跟某公司的图标颜色保持一致

--- lib/presentation/constant.dart
+++ lib/presentation/constant.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF00BF6D);
-const kSecondaryColor = Color(0xFFFE9901);
+const kPrimaryColor = Color(0xFF23AC38);
+const kSecondaryColor = Color(0xFFFEE000);
 const kContentColorLightTheme = Color(0xFF1D1D35);
 const kContentColorDarkTheme = Color(0xFFF5FCF9);
 const kWarninngColor = Color(0xFFF3BB1C);
