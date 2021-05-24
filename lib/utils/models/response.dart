6e95c8538a809ffb83dd0b9d8bf015f0ce03e495
cherys <lemonpusi@gmail.com>
2021-05-24 23:20:38 +0800

    增加一个基础的请求响应的结构体

---
+++ lib/utils/models/response.dart
@@ -0,0 +1,19 @@
+
+class Response {
+  int? code;
+  dynamic? data;
+
+  Response({this.code, this.data});
+
+  Response.fromJson(Map<String, dynamic> json) {
+    this.code = json["code"];
+    this.data = json["data"];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data["code"] = this.code;
+    data["data"] = this.data;
+    return data;
+  }
+}
