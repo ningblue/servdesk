a446f6946494b23b27bfd7214c2acdd05e6dadd9
cherys <lemonpusi@gmail.com>
2021-05-26 22:42:04 +0800

    add

--- lib/pages/portals/presentation/views/portal_view.dart
+++ lib/pages/portals/presentation/views/portal_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-// import 'package:servdesk/pages/portals/presentation/controllers/portal_controller.dart';
 import 'package:servdesk/presentation/Headers/HomeHeader.dart';
 
 class PortalView extends StatefulWidget {
